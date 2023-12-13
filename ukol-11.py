4cfbefd09cddbe4f59cdef90df53602d67a8a8ac
Karolina <k.grafova@gmail.com>
2023-12-13 21:18:46 +0100

    UKOL 11

--- ukol-11.py
+++ ukol-11.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 file_path = '/Users/karolinagerych/Desktop/platy_2021_02.csv'
 data = pd.read_csv(file_path)
 
-plt.bar(data['cislo_zamestnance'], data['plat'], color='pink', edgecolor='purple')
-plt.xlabel('Číslo zaměstnance')
-plt.ylabel('Plat')
-plt.title('Výše platů')
+bins = range(30000, 60000, 2000)
+plt.hist(data['plat'], bins=bins, edgecolor='black', alpha=1)
+plt.xlabel('Plat')
+plt.ylabel('Číslo zaměstnance')
+plt.title('Histogram')
 plt.show()
